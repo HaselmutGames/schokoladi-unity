6300e58193a23e3865305f86da945820b5b7249c
HaselmutGames <haselmut.games@gmail.com>
2025-02-21 00:53:11 +0100

    integrated new input system

--- Assets/02_Scripts/GameInput.cs
+++ Assets/02_Scripts/GameInput.cs
@@ -2,21 +2,16 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour {
 
+    private PlayerInputActions _playerInputActions;
+
+    private void Awake() {
+        _playerInputActions = new PlayerInputActions();
+        _playerInputActions.Player.Enable();
+    }
 
     public Vector2 GetMovementVectorNormalized() {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x = +1;
-        }
+        Vector2 inputVector = _playerInputActions.Player.Move.ReadValue<Vector2>();
+
         inputVector = inputVector.normalized;
         return inputVector;
     }
